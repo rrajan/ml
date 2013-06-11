4bd882c9dad7780c80a1d83ae20bec24252ee968
ravi <ravi@Ravis-MacBook-Air.local>
2013-06-10 21:22:36 -0400

    updated naiveBayes

--- naiveBayes.py
+++ naiveBayes.py
@@ -13,7 +13,23 @@ class NaiveBayes:
         self.predictions=0
         self.metrics={}
 
+    def getTable(self, fname, c_offset=0, dtype=types.IntType):
+        f = open(fname, 'r')
+        line = f.readline()
+        f.close()
+        headers = line.strip().split(",")
+
+        Y = headers[c_offset]
+        X = headers[(c_offset+1):]
+
+        mat = np.genfromtxt(fname, delimiter=",", skip_header=1, dtype=dtype, comments=types.NoneType)
+        y = mat[:, c_offset]
+        return Y,X,y,mat[:,(c_offset+1):]
+
+
     def score(self, true_labels, pos_label=1):
+        self.metrics.clear()
+
         fpr, tpr, thresholds = metrics.roc_curve(true_labels, self.predictions, pos_label)
         auc = metrics.auc(fpr,tpr)
 
@@ -21,76 +37,53 @@ class NaiveBayes:
         self.metrics['fpr'] = fpr
         self.metrics['tpr'] = tpr
 
-    def predict(self, fname, h_offset=0):
-
-        f = open(fname, 'r')
-        line = f.readline()
-        f.close()
-        headers = line.strip().split(",")
-
-        X = headers[h_offset:]
-
-        mat = np.genfromtxt(fname, delimiter=",", skip_header=1, dtype=types.IntType, comments=types.NoneType)
-
+    def predict(self, Y, X, mat):
+        self.predictions = 0
         r,c = mat.shape
-
         p_y = self.model['y1'] # prior
         pred = np.zeros(r)
         for i in range(0,r):
             y1=p_y
             y0=1.0 - p_y
-            for j in range(h_offset,c):
-                if (mat[i,j] in self.model[1][X[j-h_offset]]):
-                    y1 *= self.model[1][X[j-h_offset]][mat[i,j]]
+            for j in range(0,c):
+                if (mat[i,j] in self.model[1][X[j]]):
+                    y1 *= self.model[1][X[j]][mat[i,j]]
                 else:
-                    y1 *= self.model[1][X[j-h_offset]][self.default]
+                    y1 *= self.model[1][X[j]][self.default]
 
-                if (mat[i,j] in self.model[0][X[j-h_offset]]):
-                    y0 *= self.model[0][X[j-h_offset]][mat[i,j]]
+                if (mat[i,j] in self.model[0][X[j]]):
+                    y0 *= self.model[0][X[j]][mat[i,j]]
                 else:
-                    y0 *= self.model[0][X[j-h_offset]][self.default]
+                    y0 *= self.model[0][X[j]][self.default]
 
             pred[i] = y1 / (y1 + y0)
 
         self.predictions = pred
 
-    def caliberate(self, fname, L=1):
-
-        f = open(fname, 'r')
-        line = f.readline()
-        f.close()
-        headers = line.strip().split(",")
-
-        Y = headers[0]
-        X = headers[1:]
-
-        mat = np.genfromtxt(fname, delimiter=",", skip_header=1, dtype=types.IntType, comments=types.NoneType)
-
+    def caliberate(self, Y, X, y, mat, L=1):
+        self.model.clear()
         r,c = mat.shape
-
-        y = mat[:, 0] == 1
-
         # get counts
         x_y1_dict = {}
         x_y0_dict = {}
-        for j in range(1,c):
+        for j in range(0,c):
             kv_1 = {}
             kv_0 = {}
-            kv_1[self.default] = 1.0 * L
-            kv_0[self.default] = 1.0 * L
             for i in range(0,r):
                 if (y[i]):
                     if (mat[i,j] in kv_1):
                         kv_1[mat[i,j]] += 1
                     else:
-                        kv_1[mat[i,j]] = 1.0 * L
+                        kv_1[mat[i,j]] = L
                 else:
                     if (mat[i,j] in kv_0):
                         kv_0[mat[i,j]] += 1
                     else:
-                        kv_0[mat[i,j]] = 1.0 * L
-            x_y1_dict[X[j-1]] = kv_1
-            x_y0_dict[X[j-1]] = kv_0
+                        kv_0[mat[i,j]] = L
+            kv_1[self.default] = L
+            kv_0[self.default] = L
+            x_y1_dict[X[j]] = kv_1
+            x_y0_dict[X[j]] = kv_0
             # since idx 0 is Y
 
         # get probabilities
@@ -110,7 +103,7 @@ class NaiveBayes:
 
         ## save
 
-        self.model['y'] = y
+        self.model[Y] = y
         self.model['y_name'] = Y
         self.model['x_names'] = X
         self.model['y1'] = np.float64(sum(y)) / np.float64(len(y))
